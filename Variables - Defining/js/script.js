3456abd1386b625b4a5fa74d85a539cfd00896dc
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-05 20:17:11 -0600

    Wk2 Training Updates

---
+++ Variables - Defining/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jnix on 12/5/13.
+ */
+// Arrays
+var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]); //lets me see whats in the index #1
---
+++ assignment [Values_Variables]/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jnix on 12/5/13.
+ */
+var yearBorn = 1955;
+
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+    /*
+
+     */
+alert(yearBorn);
