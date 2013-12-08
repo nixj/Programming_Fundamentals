2a5116c4a2b8f9dcc3a385577111a60447da522d
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-07 20:58:47 -0600

    3rd Expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,3 +4,6 @@ var bottles = prompt("How many bottles are needed a day per kid?");
 console.log(bottles);
 
 var babies = prompt("How many kids are you feeding");
+
+var year = prompt("How many days are in a year?");
+console.log(year);
