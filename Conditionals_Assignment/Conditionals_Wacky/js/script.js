83fee406ccd1556ad3163ce07839b31049e26196
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 21:50:14 -0600

    First 2 variables for Wacky

---
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,5 @@
+//Johnny Nix 12-12-13 Conditionals - Wacky
+
+//calculate what movie is to be watched dependent on the presence and age of children in the group
+var children = prompt("Do you have any children present?"); //determines if children are present
+var kidsAge = prompt("Are they under the age of 5?"); //determines the ages of the children
