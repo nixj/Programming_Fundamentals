1f19fd54d4840d9d4540d7187cb391c613e6433a
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 21:50:50 -0600

    Added Movie Variable

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -3,3 +3,4 @@
 //calculate what movie is to be watched dependent on the presence and age of children in the group
 var children = prompt("Do you have any children present?"); //determines if children are present
 var kidsAge = prompt("Are they under the age of 5?"); //determines the ages of the children
+var movie;
