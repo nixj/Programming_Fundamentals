dc3d1350040232ed5d15b46afacdf7602b077043
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 21:34:16 -0600

    Industry if statement

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -2,3 +2,7 @@
 
 var age = prompt("How old are you?");
 var drink = prompt("Will you want a drink with your order?");
+
+if (age >= 55 && drink == "yes") {
+    console.log("You're meal total comes out to $6");
+}
