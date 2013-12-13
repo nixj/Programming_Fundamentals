a7d86800ca3e3d04cf9f1a3f3cbfe58ef2991c54
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 20:53:49 -0600

    Personal If statement

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -2,3 +2,7 @@
 
 var age = prompt("How old are you?"); //prompts a user to give their age
 var guardiansSigned = prompt("How many guardians signed your permission slip?"); //prompts a user to input how many parents or guardians signed the permission slip
+
+if (age >= 16 && guardiansSigned >= 1) { //calculates if a person is at least 16 AND has at least one parent's permission to attend the trip
+    console.log("Thank you. You are able to go on this trip!");  //confirms the person is eligible to go on the trip as they meet the necessary requirements
+}
