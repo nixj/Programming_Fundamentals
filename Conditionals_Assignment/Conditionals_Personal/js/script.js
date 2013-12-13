43d74cd4a971c9ab86b10ec050a3a3f5a09f8cec
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 20:55:12 -0600

    Personal Else If

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -5,4 +5,6 @@ var guardiansSigned = prompt("How many guardians signed your permission slip?");
 
 if (age >= 16 && guardiansSigned >= 1) { //calculates if a person is at least 16 AND has at least one parent's permission to attend the trip
     console.log("Thank you. You are able to go on this trip!");  //confirms the person is eligible to go on the trip as they meet the necessary requirements
+} else if (age < 16 && guardiansSigned > 1) { //calculates the requirements for persons under 16. Determines that there are at least 2 or more signatures for those under 16
+    console.log("Thank you. Since you have guardian permission, you can go on this trip!"); //confirms persons under 16 meets the requirements to go on the trip
 }
