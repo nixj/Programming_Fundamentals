c0ebf21cfbae57b01b3f3eaf14b93a281e673577
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-12 20:53:19 -0600

    Personal variables

---
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -0,0 +1,4 @@
+//Johnny Nix 12-12-13 Conditionals - Personal
+
+var age = prompt("How old are you?"); //prompts a user to give their age
+var guardiansSigned = prompt("How many guardians signed your permission slip?"); //prompts a user to input how many parents or guardians signed the permission slip
