9875ae77d326b87922931509499d62b999303335
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:59:43 -0600

    variable 1

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,13 +1,3 @@
 //Johnny Nix 12-19-2013 Functions Assignment Personal
 
-var petDog = prompt("Do you already have a dog?"); //defined variable gathering if the user has a dog
-var petCat = prompt("Do you have any cats?"); //defined variable gathering if the user has a cat
-
-function pet(x, y) { //function named and arguments set
-    var dog = x == "yes"; //variable set when owning a dog equals yes
-    var cat = y == "no"; //variable set when owning a cat equals no
-}
-
-var pets = pet(petDog, petCat); //variable setting parameter values
-
-nextPet = (petDog = "yes" || petCat == "no") ? console.log("You can't have dog!") : console.log("You can get a dog!"); //ternary that prints to the console to determine if you can have another dog
+var meals = prompt("How many meals does each soldier get a day?");
--- Ternary Operators/js/script.js
+++ Ternary Operators/js/script.js
@@ -1,14 +1,9 @@
-//Johnny Nix 12-12-13 Functions - Wacky
+var days = prompt("How many days will the mission take?");
 
-var departments = prompt("How many police departments are in your town?");
-var personnel = prompt("How many policemen are in each department?")
-
-
-
-var officerTotal = cops(departments, personnel);
-
-if (officerTotal >= 25) {
-    console.log("The town will hold a dinner in honor of your town\'s heroes because there are " + officerTotal + " people in your " + departments + " departments");
-} else {
-    console.log("There are only " + officerTotal + " people in your departments, so there will be no dinner.");
+var food = function(x, y) {
+    var food = x * y;
+    return food;
 }
+
+var mealTotal = food(meals, days);
+console.log("Each soldier has " + mealTotal + " meals for the upcoming mission.");
