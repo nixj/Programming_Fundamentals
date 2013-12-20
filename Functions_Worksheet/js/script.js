b30f2522e3f0d106b9f6937641b91e4110f07ef2
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 19:03:37 -0600

    comments for 2nd commit

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -2,4 +2,5 @@
 
 var radius = 3; //defined variable for the radius as given
 
-var total = calcCirc(2, 3.14, 3);
+var total = calcCirc(2, 3.14, 3); //defined calcCirc to reflect the formula to find the circumference of a circle 2 times Pi times the Radius
+//Also defined the result as the variable total to be printed
