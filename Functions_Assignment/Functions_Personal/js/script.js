0d67414dde954b9b918794a512f3778c50e3e5cb
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 22:04:02 -0600

    update comment 2

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,7 +1,6 @@
 //Johnny Nix 12-19-2013 Functions Assignment Personal
 
-//Johnny Nix 12-19-2013 Functions Assignment Personal
-
 var meals = prompt("How many meals does each soldier get a day?"); //defines the number of meals each soldier get a day based on input
-var days = prompt("How many days will the mission take?");
+var days = prompt("How many days will the mission take?"); //defines the number of days the mission will go on based on input
+
 
