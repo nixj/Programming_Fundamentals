f86ab9d1300640b62b4819c484bde63fc679aa0f
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 20:08:12 -0600

    added second function

--- Functions_Assignment/Function_Industry/js/script.js
+++ Functions_Assignment/Function_Industry/js/script.js
@@ -4,3 +4,10 @@ var areaRhombus = function(b, h) { //anonymous function for the area of a rhombu
     var areaR = b * h; // defines the variable areaR which calculates the base times the height of a rhombus
     return areaR; //return for the variable areaR
 }
+
+var rhombus = areaRhombus(2, 7.8); //defines the variable rhombus and the necessary arguments / parameters
+
+var perimeterTriangle = function(x, y, z) {
+    var perimeter = x + y + z;
+    return perimeter;
+}
