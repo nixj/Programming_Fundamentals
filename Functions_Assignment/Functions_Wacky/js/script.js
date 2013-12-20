e07a61943fe6d87cda91841531114bca13c92395
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:45:08 -0600

    final for wacky

--- Functions_Assignment/Functions_Wacky/js/script.js
+++ Functions_Assignment/Functions_Wacky/js/script.js
@@ -10,8 +10,8 @@ var cops = function(x, y) { //anonymous function to determine the number of pers
 
 var officerTotal = cops(departments, personnel); //defines the parameters / arguments for the officer total
 
-if (officerTotal >= 25) {
-    console.log("The town will hold a dinner in honor of your town\'s heroes because there are " + officerTotal + " people in your " + departments + " departments");
-} else {
-    console.log("There are only " + officerTotal + " people in your departments, so there will be no dinner.");
+if (officerTotal >= 25) { //if statement determining if the officer total is more than or equal to 25
+    console.log("The town will hold a dinner in honor of your town\'s heroes because there are " + officerTotal + " people in your " + departments + " departments"); //prints if total is 25 or more
+} else { //else statement if officer total is less than 25
+    console.log("There are only " + officerTotal + " people in your departments, so there will be no dinner."); //prints if total is less than 25
 }
