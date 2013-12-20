33a19a0255af82abffd05e9bf71d09e680cc8408
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:10:35 -0600

    comment

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,3 +1,3 @@
 //Johnny Nix 12-19-2013 Functions Assignment Personal
 
-var petDog = prompt("Do you already have a dog?");
+var petDog = prompt("Do you already have a dog?"); //defined variable gathering if the user has a dog
