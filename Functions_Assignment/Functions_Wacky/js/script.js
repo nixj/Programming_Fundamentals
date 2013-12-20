66ac8fe9eb925f5b1a97957c6197dda55dfb1165
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:40:19 -0600

    comments

--- Functions_Assignment/Functions_Wacky/js/script.js
+++ Functions_Assignment/Functions_Wacky/js/script.js
@@ -1,4 +1,4 @@
 //Johnny Nix 12-12-13 Functions - Wacky
 
-var departments = prompt("How many police departments are in your town?"); //defines the variable based on the number entered by teh user
-var personnel = prompt("How many policemen are in each department?")
+var departments = prompt("How many police departments are in your town?"); //defines the number of police stations based on the number entered by the user
+var personnel = prompt("How many policemen are in each department?") //defines the number of policemen in each department based on the number given by the user
