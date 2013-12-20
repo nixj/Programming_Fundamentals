d32c72b148cbeca3f146dc48170e573280551dfa
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 19:03:04 -0600

    Defined 2nd variable

--- Functions_Returning Values/js/script.js
+++ Functions_Returning Values/js/script.js
@@ -5,3 +5,6 @@ function calcCirc(d, p, n) {
     return circumference
 }
 console.log(total);
+
+//defined calcCirc to reflect the formula to find the circumference of a circle 2 times Pi times the Radius
+//Also defined the result as the variable total to be printed
--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -2,3 +2,4 @@
 
 var radius = 3; //defined variable for the radius as given
 
+var total = calcCirc(2, 3.14, 3);
