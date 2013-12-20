cb1d798d84df81bfbc0897f6b8102c3f28cdf19c
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 20:09:40 -0600

    2nd variable comments

--- Functions_Assignment/Function_Industry/js/script.js
+++ Functions_Assignment/Function_Industry/js/script.js
@@ -7,7 +7,7 @@ var areaRhombus = function(b, h) { //anonymous function for the area of a rhombu
 
 var rhombus = areaRhombus(2, 7.8); //defines the variable rhombus and the necessary arguments / parameters
 
-var perimeterTriangle = function(x, y, z) {
-    var perimeter = x + y + z;
-    return perimeter;
+var perimeterTriangle = function(x, y, z) { //anonymous function for the perimeter of a triangle
+    var perimeter = x + y + z; //defines the variable perimeter which adds together the three sides of a triangle
+    return perimeter; //return for the variable perimeter
 }
