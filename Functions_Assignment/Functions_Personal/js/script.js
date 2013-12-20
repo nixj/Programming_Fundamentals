ec55fbe9c94e512f9ad569c1eaed0ad7305ffbe2
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 22:02:29 -0600

    fix

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,4 +1,3 @@
 //Johnny Nix 12-19-2013 Functions Assignment Personal
 
-var meals = prompt("How many meals does each soldier get a day?"); //defines the number of meals each soldier get a day based on input
-var days = prompt("How many days will the mission take?");
+
--- Ternary Operators/js/script.js
+++ Ternary Operators/js/script.js
@@ -7,3 +7,10 @@ var food = function(x, y) {
 
 var mealTotal = food(meals, days);
 console.log("Each soldier has " + mealTotal + " meals for the upcoming mission.");
+
+
+
+//Johnny Nix 12-19-2013 Functions Assignment Personal
+
+var meals = prompt("How many meals does each soldier get a day?"); //defines the number of meals each soldier get a day based on input
+var days = prompt("How many days will the mission take?");
