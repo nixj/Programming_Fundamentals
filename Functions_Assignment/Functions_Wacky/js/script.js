539c44a666450ba164dd90663740716e9624971e
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:37:32 -0600

    comment 1

--- Functions_Assignment/Functions_Wacky/js/script.js
+++ Functions_Assignment/Functions_Wacky/js/script.js
@@ -1,3 +1,4 @@
 //Johnny Nix 12-12-13 Functions - Wacky
 
-var departments = prompt("How many police departments are in your town?");
+var departments = prompt("How many police departments are in your town?"); //defines the variable based on the number entered by teh user
+
