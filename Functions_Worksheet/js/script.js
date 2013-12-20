41a03f1930e450070f042cd66fca040138060d30
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 19:22:43 -0600

    added function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -5,7 +5,7 @@ var radius = 3; //defined variable for the radius as given
 var total = calcCirc(2, 3.14, 3); //defined calcCirc to reflect the formula to find the circumference of a circle 2 times Pi times the Radius
 //Also defined the result as the variable total to be printed
 
-function calcCirc(d, p, n) { //defined function calcCirc and set arguments
+function calcCirc(d, p, n) { //defined function calcCirc and set parameters
     var circumference = d * p * n; //defined variable to calculate the circumference
     return circumference //added return to reflect the circumference
 }
@@ -16,4 +16,5 @@ var weight = 125; //defined variable for the victim's weight
 var total = calcStings(weight, 8.666666687); //defined calcStings to reflect the victim's weight and the number of stings needed to kill
 //Also defined the result as the variable total to be printed
 
-
+function calcStings(w, s) { //defined function calcStings and set parameters
+}
