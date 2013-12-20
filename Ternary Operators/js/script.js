ca59895c25b210d28f18814c48367d3d46535227
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 21:09:49 -0600

    first for personal

--- Ternary Operators/js/script.js
+++ Ternary Operators/js/script.js
@@ -1,16 +1,10 @@
-var rhombus = areaRhombus(2, 7.8);
+var petCat = prompt("Do you have any cats?");
 
-var perimeterTriangle = function(x, y, z) {
-    var perimeter = x + y + z;
-    return perimeter;
+function pet(x, y) {
+    var dog = x == "yes";
+    var cat = y == "no";
 }
 
-var triangle = perimeterTriangle(5, 8, 12);
+var pets = pet(petDog, petCat);
 
-if (rhombus == triangle) {
-    console.log("Congratulations, the area of your rhombus " + rhombus + "inches is equal to the perimeter of your triangle " + triangle + "inches.");
-} else if (rhombus > triangle) {
-    console.log("Your rhombus which is equal to " + rhombus + " inches, is larger than the perimeter of your triangle which is " + triangle + " inches.")
-} else {
-    console.log("Your rhombus which is equal to " + rhombus + " inches, is smaller than the perimeter of you triangle which is " + triangle + " inches.")
-}
+nextPet = (petDog = "yes" || petCat == "no") ? console.log("You can't have dog!") : console.log("You can get a dog!");
