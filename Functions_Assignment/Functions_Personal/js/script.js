6627beb9d2ac850891867532dce4e0f179367ce1
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 22:04:44 -0600

    function and comment

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -3,4 +3,7 @@
 var meals = prompt("How many meals does each soldier get a day?"); //defines the number of meals each soldier get a day based on input
 var days = prompt("How many days will the mission take?"); //defines the number of days the mission will go on based on input
 
-
+var food = function(x, y) { //defines the function food and sets parameters / arguments
+    var food = x * y;
+    return food;
+}
