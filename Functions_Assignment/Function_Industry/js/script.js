86413d26bbacd04795812195c5d19a329484d600
nixj <AssassinzAngel@Johnnys-MacBook-Pro.local>
2013-12-19 20:07:02 -0600

    comments for 1st variable

--- Functions_Assignment/Function_Industry/js/script.js
+++ Functions_Assignment/Function_Industry/js/script.js
@@ -1,6 +1,6 @@
 //Johnny Nix 12-12-13 Functions - Industry
 
-var areaRhombus = function(b, h) {
-    var areaR = b * h;
-    return areaR;
+var areaRhombus = function(b, h) { //anonymous function for the area of a rhombus
+    var areaR = b * h; // defines the variable areaR which calculates the base times the height of a rhombus
+    return areaR; //return for the variable areaR
 }
